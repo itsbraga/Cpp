61c9d9c3e30af05f2a0c04f8da14c1d630af41f0
Ana <it.braga@pm.me>
2025-02-06 00:36:46 +0100

    esthetic

--- C00/ex01/PhoneBook.hpp
+++ C00/ex01/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: art3mis <art3mis@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 19:30:55 by annabrag          #+#    #+#             */
-/*   Updated: 2025/01/27 18:57:01 by art3mis          ###   ########.fr       */
+/*   Updated: 2025/02/05 15:48:08 by art3mis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,25 +33,22 @@
 # include "custom.hpp"
 
 /******************************************************************************\
- * TOOLS
-\******************************************************************************/
-
-bool	checkInput(std::string input, std::string field);
-
-/******************************************************************************\
  * CLASSES
 \******************************************************************************/
 
 class Contact
 {
 	public:
+	
 		Contact(void);
 		~Contact(void);
 
 		void			setContactInfo(std::string target, std::string newValue);
 		std::string		getContactInfo(std::string target) const;
 
+
 	private:
+
 		std::string 	first_name;
 		std::string 	last_name;
 		std::string 	nickname;
@@ -62,6 +59,7 @@ class Contact
 class PhoneBook
 {
 	public:
+	
 		PhoneBook(void);
 		~PhoneBook(void);
 
@@ -71,10 +69,18 @@ class PhoneBook
 		void		showContactList(void);
 		void		showContact(size_t choice);
 
+
 	private:
+	
 		Contact		array[MAX_CONTACTS];
 		size_t		head;
 		int			contactAmount;
 };
+
+/******************************************************************************\
+ * TOOLS
+\******************************************************************************/
+
+bool	checkInput(std::string input, std::string field);
 
 #endif
