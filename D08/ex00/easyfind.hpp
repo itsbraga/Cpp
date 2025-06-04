8586caf1a05796c64ccad19cd86d7354c75a5255
Anna BRAGA <annabrag@bess-f2r1s15.clusters.42paris.fr>
2025-06-04 19:51:57 +0200

    beginning of D08/ex00

---
+++ D08/ex00/easyfind.hpp
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: annabrag <annabrag@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/04 16:14:06 by annabrag          #+#    #+#             */
+/*   Updated: 2025/06/04 19:50:52 by annabrag         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+/******************************************************************************\
+ * LIBRARIES
+\******************************************************************************/
+
+# include <iostream>
+# include <algorithm>
+# include <stdexcept>
+# include "../colors.hpp"
+
+/******************************************************************************\
+ * LIBRARIES
+\******************************************************************************/
+
+# define SUCCESS 0
+# define FAILURE 1
+
+/******************************************************************************\
+ * TEMPLATE FUNCTIONS
+\******************************************************************************/
+
+template< typename T >
+typename T::iterator	easyfind(T& container, int value)
+{
+	T::iterator it = std::find(container.begin(), container.end(), value);
+	if (it == container.end())
+		throw std::runtime_error("Value not found in container");
+	return (it);
+}
+
+template< typename T >
+typename T::const_iterator	easyfind(const T& container, int value)
+{
+	T::const_iterator it = std::find(container.begin(), container.end(), value);
+	if (it == container.end())
+		throw std::runtime_error("Value not found in container");
+	return (it);
+}
+
+#endif
