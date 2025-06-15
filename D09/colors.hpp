7aa62ca36179f27b8a277f6bf3d13dbd65d178b1
abraga42 <86627490+abraga42@users.noreply.github.com>
2025-06-16 00:43:43 +0200

    prepared D09

---
+++ D09/colors.hpp
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   colors.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: panther <panther@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/06 20:00:51 by annabrag          #+#    #+#             */
+/*   Updated: 2025/06/15 23:04:13 by panther          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef COLORS_HPP
+# define COLORS_HPP
+
+/******************************************************************************\
+ * COLORS
+\******************************************************************************/
+
+# define RESET  		"\e[0m"
+
+# define BOLD			"\e[1m"
+# define ITAL			"\e[3m"
+# define UNDERLINE		"\e[4m"
+
+# define BLACK			"\e[30m"
+# define GRAY			"\e[90m"
+# define RED			"\e[31m"
+# define GREEN			"\e[32m"
+# define YELLOW			"\e[33m"
+# define SAND			"\e[38;2;231;200;160m"
+# define DARK_SAND		"\e[38;2;212;163;115m"
+# define ORANGE			"\e[38;5;208m"
+# define BLUE			"\e[34m"
+# define PURPLE			"\e[35m"
+# define HOT_PINK		"\e[38;2;255;105;180m"
+# define PINK			"\e[38;2;255;182;193m"
+# define CYAN			"\e[36m"
+# define WHITE			"\e[37m"
+
+# define LIGHT_GRAY		"\e[38;2;173;165;165m"
+# define LIGHT_GRAY2	"\e[38;2;211;211;211m"
+
+// Pastel colors
+# define PG				"\e[38;2;173;235;179m" // green
+# define PGG			"\e[38;2;152;168;105m" // green
+# define PB				"\e[38;2;179;235;242m" // blue
+# define PY				"\e[38;2;255;234;150m" // yellow
+# define PP				"\e[38;2;211;211;255m" // purple
+# define PO				"\e[38;2;255;178;127m" // orange
+
+#endif
